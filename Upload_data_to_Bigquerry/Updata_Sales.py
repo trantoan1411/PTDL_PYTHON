36529ff8189aa84e719b56dc5be46edd80e7897b
trantoan1411 <125675164+trantoan1411@users.noreply.github.com>
2025-06-29 01:13:32 +0700

    Update Updata_Sales.py

--- Upload_data_to_Bigquerry/Updata_Sales.py
+++ Upload_data_to_Bigquerry/Updata_Sales.py
@@ -4,7 +4,7 @@ import pandas as pd
 import os
 
 # Kết nối đến BigQuery thông qua credentials
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "powerbi-2474725-83fdee3ff325.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "Config.json"
 
 # Tạo client BigQuery
 client = bigquery.Client()
@@ -13,7 +13,7 @@ client = bigquery.Client()
 table_id = "powerbi-2474725.PowerBI_Data.Sales"
 
 # Đọc dữ liệu từ file CSV
-df = pd.read_csv("D:/Power_BI_PL300_Datapot/Dataset/LAB_2/OneDrive_1_4-21-2025/USSales/sales.csv")
+df = pd.read_csv("sales.csv")
 
 # Upload dữ liệu lên bảng đã tạo
 job = client.load_table_from_dataframe(df, table_id)
